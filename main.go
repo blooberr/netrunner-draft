a2b54094459093c5007990a5aff5bc0a2b1d40b6
blooberr <blooberr@gmail.com>
2014-05-18 23:04:54 -0700

    left out the actual program

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	//"fmt"
+	"net/http"
+
+	"github.com/blooberr/netrunner-draft/draft"
+)
+
+func main() {
+	server := draft.NewServer()
+	go server.Launch()
+
+	// can split this off into a separate service (rails, node.js, whatever.)
+	http.Handle("/", http.FileServer(http.Dir("public/")))
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		panic(err)
+	}
+}
