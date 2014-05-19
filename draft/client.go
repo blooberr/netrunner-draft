ce369f7d5e4fa18dc51ed284b6e2b5f44fbe31ee
blooberr <blooberr@gmail.com>
2014-05-19 00:04:26 -0700

    adding broadcast, onmessage fetch, player names

--- draft/client.go
+++ draft/client.go
@@ -6,10 +6,35 @@ import (
 )
 
 type Client struct {
-	Ws *websocket.Conn
+	Ws         *websocket.Conn
+	PlayerName string
+	WriteChan  chan CommandPacket
 }
 
-func NewClient(ws *websocket.Conn) *Client {
+func NewClient(ws *websocket.Conn, playerName string) *Client {
 	fmt.Printf("NewClient: %#v \n", ws)
-	return &Client{Ws: ws}
+	writeChan := make(chan CommandPacket, 10)
+	return &Client{Ws: ws, PlayerName: playerName, WriteChan: writeChan}
+}
+
+func (c *Client) Launch() {
+	c.listenWrite()
+}
+
+// Write - lazy short cut way.  I don't think this actully saves any time, but 
+// will feel more intuitive
+func (c *Client) Write(cp CommandPacket) {
+  c.WriteChan <- cp
+}
+
+func (c *Client) listenWrite() {
+	fmt.Printf("client.listenwrite \n")
+	for {
+		select {
+		// send message to the client
+		case msg := <-c.WriteChan:
+			fmt.Printf("receive: %#v \n", msg)
+			websocket.JSON.Send(c.Ws, msg)
+		}
+	}
 }
--- draft/server.go
+++ draft/server.go
@@ -22,8 +22,11 @@ func (s *Server) Launch() {
 	fmt.Printf("Launch: running websocket server\n")
 
 	onConnect := func(ws *websocket.Conn) {
-		client := NewClient(ws)
+		numClients := len(s.Clients)
+		playerName := fmt.Sprintf("player-%d", numClients)
+		client := NewClient(ws, playerName)
 		s.AddClientChannel <- client
+    client.Launch()
 		defer ws.Close()
 	}
 
@@ -32,8 +35,28 @@ func (s *Server) Launch() {
 	for {
 		select {
 		case newClient := <-s.AddClientChannel:
+			fmt.Printf("Adding new client: %#v \n", newClient)
+
+			newNameMap := make(map[string]string)
+			newNameMap["name"] = newClient.PlayerName
+      cp := CommandPacket{Event: "New Player", Data: newNameMap}
+
+			fmt.Printf("sending on writechan: %#v\n", cp)
+			newClient.Write(cp)
+
 			s.Clients = append(s.Clients, newClient)
 			fmt.Printf("%d are in the lobby.\n", len(s.Clients))
+
+      cp = CommandPacket{Event: "New Player Joined", Data: newNameMap }
+      s.Broadcast(cp)
 		}
 	}
+}
+
+// Broadcast sends to all clients connected
+func (s *Server) Broadcast(cp CommandPacket) {
+  for id, client := range s.Clients {
+    fmt.Printf("[%d] %#v \n", id, client)
+    client.Write(cp)
+  }
 }
